8dcee858adb2201372bc17ad58bd777daad29db6
MKMalik <engimkmalik@gmail.com>
2020-11-04 20:58:14 +0530

    first commit

--- api.js
+++ api.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var todoController = require('./controllers/todo-controller');
+const cors = require('cors');
 
 var app = express();
 
+//Middleware
+app.use(cors())
+
 todoController(app);
 
-const port = 5500;
-const ip = 'localhost';
+const port = 3000;
+const ip = '192.168.43.4';
 
 app.listen(port, ip, () => {
     console.log(`Server started listening at ${ip}:${port}`);
--- controllers/todo-controller.js
+++ controllers/todo-controller.js
@@ -1,35 +1,51 @@
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var os = require('os');
 
-var urlencodedParser = bodyParser.urlencoded({entended: false});
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({extended: true});
 
-mongoose.connect('mongodb://localhost:27017/todo-app?retryWrites=true&w=majority', function(err) {
+mongoose.connect('mongodb://localhost:27017/todo-app', function(err) {
      if (err) { throw err;  console.log('DB Connection problem');}
      console.log('DB Connected!');
 });
 
 //create schema
 var todoSchema = new mongoose.Schema({
-    item: String,
+    item: {
+        type: String,
+        minlength: 1,
+        required: true,
+    },
     timeStamp: String,
 });
 
 var Todo = mongoose.model('Todo', todoSchema); //2nd todo, collection name
-const todo = new Todo();
 
 
 module.exports = function (app) {
-    app.get('/api', (req, res) => {
-        const data = Todo.find({}, (err, docs) => {
-            res.json(docs);
-        });
+
+    app.get('/api', async (req, res) => {
+        try {
+            await Todo.find({}, (err, docs) => {
+                res.json(docs);
+            });
+        } catch (error) {
+            res.json({message: error});
+        }
     });
 
-    app.post('/api', urlencodedParser, (req, res) => {
-        Todo(req.body).save(function(err, data) {
-            if (err) throw err;
-        });
+    app.post('/api', jsonParser, async (req, res) => {
+        console.log(req.body);
+        try {
+            await Todo(req.body).save(function(err, doc) {
+                console.log(doc);
+                res.send('success');
+            });
+        } catch (error) {
+            res.json({message: error});
+        }
     });
+
+    // app.use(jsonParser);
 }
